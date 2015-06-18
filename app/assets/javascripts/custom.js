069396b2ad05741dc5c39876cd5e8f6309fe5b06
Shahriar-ahmmed <tuhinshahriar@gmail.com>
2015-06-18 02:07:49 +0000

    page scroll added

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,19 +1,19 @@
-$(function () {
-    $('a.page-scroll').bind('click', function (event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-
-        /* if ($(".scroll-navigation li").hasClass("active")) {
-         $(".scroll-navigation li").removeClass('active');
-         }
-         $(this).parent('li').addClass('active');*/
-        console.log($($anchor.attr('href')).offset().top);
-        event.preventDefault();
-
-    });
-});
+//$(function () {
+//    $('a.page-scroll').bind('click', function (event) {
+//        var $anchor = $(this);
+//        $('html, body').stop().animate({
+//            scrollTop: $($anchor.attr('href')).offset().top
+//        }, 1500, 'easeInOutExpo');
+//
+//        /* if ($(".scroll-navigation li").hasClass("active")) {
+//         $(".scroll-navigation li").removeClass('active');
+//         }
+//         $(this).parent('li').addClass('active');*/
+//        console.log($($anchor.attr('href')).offset().top);
+//        event.preventDefault();
+//
+//    });
+//});
 $(document).ready(function(){
     $('.nav').on('click','li', function(){
         $(this).siblings().removeClass('active');
