9f26569b74da73ffb0ddcd79d1c655ffd7dbbdb9
shahriar <tuhinshahriar@gmail.com>
2016-02-21 02:54:09 +0600

    navbar complete with new look

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,6 +1,13 @@
-//$(document).ready(function(){
-//    $('.nav').on('click','li', function(){
-//        $(this).siblings().removeClass('active');
-//        $(this).addClass('active');
-//    });
-//});
+$(document).ready(function(){
+
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 50){
+            $(".navbar-fixed-top").addClass("navbar-shrink");
+        }
+        else {
+            $(".navbar-fixed-top").removeClass("navbar-shrink");
+        }
+    });
+
+});
+
--- app/assets/javascripts/scrolling-nav.js
+++ app/assets/javascripts/scrolling-nav.js
@@ -1,16 +1,15 @@
 //jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(document).scrollTop() > 50){
-        $(".navbar-fixed-top").addClass("navbar-shrink");
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
-        $(".navbar-fixed-top").removeClass("navbar-shrink");
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
 });
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
-//    have to work with page scroll    $(".navbar .nav > li > a::after").toggleClass('full-width');
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
