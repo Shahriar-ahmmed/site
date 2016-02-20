6e05f8594e5c1c9c4f9ed1787af0af80bf6adc3a
shahriar <tuhinshahriar@gmail.com>
2016-02-20 15:00:32 +0600

    modified navbar color on 20.02.16

--- app/assets/javascripts/scrolling-nav.js
+++ app/assets/javascripts/scrolling-nav.js
@@ -10,6 +10,7 @@ $(window).scroll(function() {
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
+//    have to work with page scroll    $(".navbar .nav > li > a::after").toggleClass('full-width');
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
